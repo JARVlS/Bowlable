830fa6ce1eaab252bc7c687811cb242dd5c54268
Timo Stolz <timo.stolz@gmail.com>
2023-05-24 10:36:16 +0200

    allow vite to use tsconfig path aliases

--- @app/client/vite.config.ts
+++ @app/client/vite.config.ts
@@ -2,11 +2,13 @@ import Vue from "@vitejs/plugin-vue";
 import { defineConfig } from "vite";
 import Pages from "vite-plugin-pages";
 import viteSSR from "vite-ssr/plugin.js";
+import tsconfigPaths from "vite-tsconfig-paths";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     viteSSR(),
+    tsconfigPaths(),
     Vue({
       include: [/\.vue$/, /\.md$/],
       template: {
